b4e615c9b6f73d0de57fc39d45bbdda45d1869dd
jonhuynh <jonvictory@gmail.com>
2019-08-30 17:25:11 -0600

    adding word bank test

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,3 +5,12 @@ var userElemental = document.getElementById("userElemental");
 var winsElemental = document.getElementById("winsElemental");
 var lossElemental = document.getElementById("lossElemental");
 
+//setting up words and word mechanics
+var werds = [
+    "snowcrash",
+    "cyberspace",
+    "shadowrun",
+    "countzero",
+    "wilson"
+]
+
