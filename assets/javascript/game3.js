b1b38a9e7ac66e27cce00657bdd32094494b6e98
jonhuynh <jonvictory@gmail.com>
2019-09-01 15:57:43 -0600

    i have no idea

--- assets/javascript/game3.js
+++ assets/javascript/game3.js
@@ -13,16 +13,30 @@ var werds = [
     "countzero",
     "wilson"
 ]
+//test
+var check = ["c", "h", "e", "k"]
+var test = ["l"]
+function testy(){
+    return "l";
+}
+console.log(testy())
+function testcheck(){
+    console.log(check.findIndex(testy))
+}
+testcheck()
 
+
+//test
+ 
 var werd = werds[Math.floor(Math.random() * werds.length)];
-
 var ansKey = [];
-var answer = []
 for (var i = 0; i < werd.length; i++) {
     ansKey[i] = "_";
+
 }
 
 var playerGuessed = []
+lossElemental.textContent = playerGuessed + "ASSHAT";
 var remainder = werd.length;
 var remainTest = document.getElementById("winsElemental")
 remainTest.textContent = remainder + " fuck you"
@@ -33,61 +47,89 @@ ansKeySel.textContent = "Your Word: " + ansKey.join(" ");
 remainderGrab = document.getElementById("remainder");
 remainderGrab.textContent = remainAttempts + "Fuck";
 
-
-
+splizit();
+function splizit() {
+    var splitter = werd.split("");
+    return splitter;
+}
+var werdWerd = splizit();
 
 function input() {
     document.onkeyup = function (event) {
-        
         var playerInput = event.key;
         playerGuess = playerInput.toLowerCase();
-        
+        directElemental.textContent = "Your Guess: " + playerGuess;
+        //
+        //run event.key dependant functions
+        //
         guessed();
-        lossElemental.textContent = playerGuessed + "ASSHAT";
+        werdCheckLoopPos();
+        werdCheckLoopNeg();
 
+        //
+        //define event.key dependant functions
+        //
+        function dummyPlayerGuess() {
+            return playerGuess;
+            
+            
+        }
         function guessed() {
             var keyPress;
             if (typeof event !== 'undefined') {
                 keyPress = event.keyCode;
-              }
-              else if (e) {
+            }
+            else if (e) {
                 keyPress = e.which;
-              }
-            
-              playerGuessed.push(String.fromCharCode(keyPress));
-            
-              return false;   // Prevents the default action
-            };
+            }
+
+            playerGuessed.push(String.fromCharCode(keyPress));
+
+            return false;
+        };
         
 
-        directElemental.textContent = "Your Guess: " + playerGuess;
-        
-        for (var j = 0; j < werd.length; j++) {
+        function werdCheckLoopPos() {
+            for (var j = 0; j < werd.length; j++) {
 
-            if (werd[j] === playerGuess) {
-                werdUpdate();
-                }  
+                if (werd[j] === playerGuess) {
+                    ansKey[j] = playerGuess;
+                    remainder--;
+                    ansKeySel.textContent = "Your Word: " + ansKey.join(" ");
+                    remainTest.textContent = remainder + " fuck you";
+                }
+            }
+        };
+
+        function werdCheckLoopNeg() {
+            if (werdWerd.findIndex(dummyPlayerGuess) === -1) {
+                remainAttempts--;
+                remainderGrab.textContent = remainAttempts + "Fuck";
         }
-        function werdUpdate() {
-            ansKey[j] = playerGuess;
-            remainder--;
-            ansKeySel.textContent = "Your Word: " + ansKey.join(" ");
-            remainTest.textContent = remainder + " fuck you"
+        function fuck(){
+           return werdWerd.findIndex(dummyPlayerGuess);
         }
-        // }
+        console.log(fuck())
+    }
+        console.log(dummyPlayerGuess() +"     dummyfunctionfuck");
+        console.log(werdWerd.findIndex(dummyPlayerGuess));
+        console.log(werd)
+        console.log(werdWerd)
+        console.log(dummyPlayerGuess)
+        
         //function attemptUpdate() {
-           // remainAttempts--;
-           // remainderGrab.textContent = remainAttempts + "Fuck";
+        // remainAttempts--;
+        // remainderGrab.textContent = remainAttempts + "Fuck";
 
         //}
         // else if (answer.findIndex(playerGross) === -1) {
-                //     remainAttempts--;
-                //     remainderGrab.textContent = remainAttempts + "Fuck";
-                // }
+        //     remainAttempts--;
+        //     remainderGrab.textContent = remainAttempts + "Fuck";
+        // }
     }
 }
 
-    input();
+input();
 
 //playerinput
 
